3cea39df1e7bdad74177c88d4045c9be2d718716
wookie1380 <cmk1380@gmail.com>
2023-07-02 23:27:45 -0400

    Initial Upload
    multithread counter vs single thread counter

---
+++ MultithreadCounter/src/threadCounter/Main.java
@@ -0,0 +1,17 @@
+package threadCounter;
+
+/*
+
+Implement a parallel array sum, and compare performance with single thread sum. 
+This is not an easy assignment – just do as much as you can and turn in what you have for partial credit.
+
+Make an array of 200 million random numbers between 1 and 10. 
+Compute the sum in parallel using multiple threads. 
+Then compute the sum with only one thread, and display the sum and times for both cases.
+
+*/
+
+public class Main extends Thread{
+
+	
+}
